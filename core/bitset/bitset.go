dfa143816f3abb745b92c6c35dc70ca294cc234c
Alexander Gusev <gussev@hotmail.com>
2021-09-01 10:02:54 +0300

    to add more comments

--- core/bitset/bitset.go
+++ core/bitset/bitset.go
@@ -2,8 +2,11 @@ package bitset
 
 // BitSet functions to work with simplified picture
 type BitSet interface{
+	// Get value of the index in bitset
 	Get(index int) bool
+	// Set value for the index in bitset
 	Set(index int, value bool)
+	// Len of the bitset
 	Len() int
 }
 
